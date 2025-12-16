52d159452dda965d7d8ec92b6a74d6f88d18fea2
jyrby <133908902+fatherlessjoe@users.noreply.github.com>
2025-12-16 10:56:28 -0500

    Add db connection

--- victoria-api/index.js
+++ victoria-api/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
-const sequelize = require('sequelize');
+import express from 'express';
+import { Sequelize, DataTypes } from 'sequelize';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
+// DB configuration
+
+const sequelize = new Sequelize(process.env.URI_DB_DEV, {
+  dialect: 'postgres'
+});
 
 const app = express();
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 // App configuration
 
@@ -10,3 +28,8 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log('Server Started at ${3000}')
 }); 
+
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
